7518502c5bc7f70c7113e6f0748d54806494c413
Mathias <mathiasbergqvist88@gmail.com>
2020-09-11 14:39:41 +0200

    Setup routing with svelte-routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,47 @@
 <script>
-	const applicationName = "Football Quiz";
+  import { Route, Router, Link } from "svelte-routing";
+  import Home from "./views/Home.svelte";
+  import Game from "./views/Game.svelte";
+  import Scoreboard from "./views/Scoreboard.svelte";
+
+  export let url = "";
 </script>
 
-<main>
-	<h1>Welcome to {applicationName}!</h1>
-	<button>New Game</button>
-	<button>Scoreboard</button>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<Router {url}>
+  <main>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="game">New Game</Link>
+      <Link to="scoreboard">Scoreboard</Link>
+    </nav>
+    <div>
+      <Route path="game" component={Game} />
+      <Route path="scoreboard" component={Scoreboard} />
+      <Route path="/">
+        <Home />
+      </Route>
+    </div>
+  </main>
+</Router>
---
+++ src/views/Game.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {data} from '../data';
+    let score = 0;
+</script>
+
+<h1>{data[0].name}</h1>
