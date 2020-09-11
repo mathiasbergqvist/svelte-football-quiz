daec4ced055e49c12a052354361a37a534ef2d6e
Mathias <mathiasbergqvist88@gmail.com>
2020-09-11 16:07:13 +0200

    Helpers for correcting team score

--- src/views/Game/helpers.js
+++ src/views/Game/helpers.js
@@ -2,20 +2,40 @@ import leven from 'leven';
 
 export const CORRECT_ANSWER_POINTS = 100;
 
-export const isCorrectSpelling = (string1, string2) => {
+const isCorrectSpelling = (string1, string2) => {
     const trimmedString1 = string1.toLowerCase().trim();
     const trimmedString2 = string2.toLowerCase().trim();
     const levenScore = leven(trimmedString1, trimmedString2);
     return levenScore <= 3;
 };
 
-export const isCorrectPlayer = (player, key, answers, squad) => {
-    const squadPlayer = getPlayer(squad, player.player);
-    const answer = answers[key];
-    const playerLastName = getLastName(squadPlayer);
-    return isCorrectSpelling(playerLastName, answer);
-};
+const isCorrectPlayer = (answerPlayer, correctAnswerPlayer) => (
+    isCorrectSpelling(answerPlayer, correctAnswerPlayer)
+);
 
-export const correctGame = () => {
+const isCorrectManager = (answerManager, team) => (
+    isCorrectSpelling(answerManager, team.manager)
+);
 
+const isCorrectArena = (answerArena, team) => (
+    isCorrectSpelling(answerArena, team.arena)
+); 
+
+export const correctTeamScore = (answerManager, answerArena, answerPlayers, team) => {
+    let score = 0;
+    if (isCorrectManager(answerManager, team)) {
+        score+= CORRECT_ANSWER_POINTS;
+    }
+
+    if (isCorrectArena(answerArena, team)) {
+        score+= CORRECT_ANSWER_POINTS;
+    }
+
+    answerPlayers.forEach((player, i) => {
+        if(isCorrectPlayer(player, team.players[i].name)) {
+            score += CORRECT_ANSWER_POINTS;
+        }
+    });
+    
+    return score;
 };
