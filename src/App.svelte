a651ceaa315806bb2fecacf8b60fa3986cf4b5d9
Mathias <mathiasbergqvist88@gmail.com>
2020-09-11 14:24:54 +0200

    Project setup

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	const applicationName = "Football Quiz";
+</script>
+
+<main>
+	<h1>Welcome to {applicationName}!</h1>
+	<button>New Game</button>
+	<button>Scoreboard</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const data = {
+    name: 'Manchester United',
+    manager: 'Ole-Gunnar',
+    arena: 'Old Trafford',
+    players: [
+        {
+            name: 'Foo 1',
+            number: '1',
+            county: '🇯🇲'
+        },
+        {
+            name: 'Foo 2',
+            number: '2',
+            county: '🇯🇲'
+        },
+        {
+            name: 'Foo 3',
+            number: '1',
+            county: '🇯🇲'
+        }
+    ]
+};
