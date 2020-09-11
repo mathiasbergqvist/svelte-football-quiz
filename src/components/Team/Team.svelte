1d53c33e18a3d3d1bd83a2c92084058e35229fc6
Mathias <mathiasbergqvist88@gmail.com>
2020-09-11 17:13:15 +0200

    Game Over component

--- src/components/Team/Team.svelte
+++ src/components/Team/Team.svelte
@@ -25,17 +25,19 @@
   };
 </script>
 
-<h2>{team.name}</h2>
-<form on:submit={handleSubmit}>
-  <label for="manager">Manager</label>
-  <input type="text" name="manager" bind:value={answerManager} />
-  <label for="arena">Arena</label>
-  <input type="text" name="arena" bind:value={answerArena} />
-  {#each team.players as player, i}
-    <label for={`player-${i}`}>{player.number}
-      <span>{player.county}</span>
-      {player.position}</label>
-    <input type="text" name={`player-${i}`} bind:value={answerPlayers[i]} />
-  {/each}
-  <button type="Submit">Submit answers</button>
-</form>
+{#if team !== undefined}
+  <h2>{team.name}</h2>
+  <form on:submit={handleSubmit}>
+    <label for="manager">Manager</label>
+    <input type="text" name="manager" bind:value={answerManager} />
+    <label for="arena">Arena</label>
+    <input type="text" name="arena" bind:value={answerArena} />
+    {#each team.players as player, i}
+      <label for={`player-${i}`}>{player.number}
+        <span>{player.county}</span>
+        {player.position}</label>
+      <input type="text" name={`player-${i}`} bind:value={answerPlayers[i]} />
+    {/each}
+    <button type="Submit">Submit answers</button>
+  </form>
+{/if}
--- src/views/Game/Game.svelte
+++ src/views/Game/Game.svelte
@@ -2,9 +2,14 @@
   import { data } from "../../data";
   import { correctTeamScore } from "../../components/Team/helpers";
   import Team from "../../components/Team/Team.svelte";
+  import GameOver from "../../components/GameOver/GameOver.svelte";
   import { score, round } from "../../store";
 </script>
 
-<h1>Team {$round + 1} of {data.length}</h1>
-<Team team={data[$round]} />
+{#if round === data.length}
+  <GameOver />
+{:else}
+  <h1>Team {$round + 1} of {data.length}</h1>
+  <Team team={data[$round]} />
+{/if}
 <div>Score: {$score}</div>
