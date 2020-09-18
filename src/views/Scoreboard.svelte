ec9c7b1dc14d17d37b3bd3985ca78771651e1c08
Mathias <mathiasbergqvist88@gmail.com>
2020-09-18 13:55:09 +0200

    Setup scoreboard table

--- src/views/Scoreboard.svelte
+++ src/views/Scoreboard.svelte
@@ -1,1 +1,20 @@
+<script>
+import { onMount } from 'svelte';
+
+let scoreboardData = [];
+
+onMount(async () => {
+    const res = await fetch(`${API_ENDPOINT}/scores`);
+    const resJson = await res.json();
+    scoreboardData = resJson.sort((a, b) => a.score - b.score).reverse();
+});
+</script>
+
 <h1>Scoreboard</h1>
+<ul>
+    {#each scoreboardData as score}
+        <li><span>{score.name}</span> <span>{score.score}</span></li>
+    {:else}
+        <h2>Scoreboard loading...</h2>
+    {/each}
+</ul>
